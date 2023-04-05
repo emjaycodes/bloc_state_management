4c696d9a0bba9e352ec3840923c7d24cf18c35e5
emjaycodes <justicenwogu8@gmail.com>
2023-04-05 10:09:30 -0700

    removed todos

--- lib/bloc/color_bloc.dart
+++ lib/bloc/color_bloc.dart
@@ -8,19 +8,15 @@ class ColorBloc extends Bloc<ColorEvent, ColorState> {
   bool initState = true;
   ColorBloc() : super(ColorInitial()) {
     on<ColorEvent>((event, emit) {
-      // TODO: implement event handler
     });
     on<InitialEvent>((event, emit) {
-      // TODO: implement event handler
       emit(ColorUpdateState(initialState: initState));
     });
     on<ColorToBlue>((event, emit) {
-      // TODO: implement event handler
       initState = true;
       emit(ColorUpdateState(initialState: initState));
     });
     on<ColorToRed>((event, emit) {
-      // TODO: implement event handler
       initState = false;
       emit(ColorUpdateState(initialState: initState));
     });
